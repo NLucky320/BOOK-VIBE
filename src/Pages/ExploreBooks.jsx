3a4de6e37e55a0f5576847aefc5784557b1425b0
Nlucky320 <luckyes144@gmail.com>
2024-03-28 02:23:00 +0600

    some minor changes

--- src/Pages/ExploreBooks.jsx
+++ src/Pages/ExploreBooks.jsx
@@ -11,12 +11,14 @@ const ExploreBooks = () => {
   }, [data]);
 
   return (
-    <div className="p-2 mt-[80px] text-center m-4">
-      <h2 className="font-bold text-[40px]">Explore Books</h2>
+    <div className=" mt-[80px] p-2 text-center">
+      <div className=" m-4">
+        <h2 className="font-bold text-[40px]">Explore Books</h2>
+      </div>
       <div className="hero bg-[#0d0d0d0d] p-4 mt-6 md:mt-[52px] md:py-[80px] rounded-[24px]">
         <div className="hero-content flex-col lg:flex-row-reverse gap-4 lg:gap-[86px] pr-4">
-          <img src={image} className="max-w-[310px] rounded-lg shadow-2xl" />
-          <div className="max-w-[526px]">
+          <img src={image} className="max-w-[280px] rounded-lg shadow-2xl" />
+          <div className="max-w-[500px]">
             <h1 className="text-5xl font-bold pb-10">
               {" "}
               Hundreds of books picked for you
@@ -27,7 +29,6 @@ const ExploreBooks = () => {
           </div>
         </div>
       </div>
-
       <div className="mt-6 md:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {books.map((book) => (
           <ExploreBook key={book.bookId} book={book}></ExploreBook>
