f4a930b3fe1f3438e9293c503514d7d646da50a6
Nlucky320 <luckyes144@gmail.com>
2024-03-28 02:15:30 +0600

    some minor changes

--- src/Pages/BookDetails.jsx
+++ src/Pages/BookDetails.jsx
@@ -5,7 +5,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { saveBooks } from "../Utils";
 import useBooksData from "../Hooks/useBooksData";
 import { useEffect, useState } from "react";
@@ -15,26 +15,35 @@ const BookDetails = () => {
   const { data } = useBooksData();
   const [singleData, setSingleData] = useState({});
   const { id } = useParams();
-//   console.log(id, singleData);
- 
-    
+  //   console.log(id, singleData);
+
   useEffect(() => {
-  const singleData = data.find((item) => item.bookId == id);
-  setSingleData(singleData);
-}, [data, id]);
+    const singleData = data.find((item) => item.bookId == id);
+    setSingleData(singleData);
+  }, [data, id]);
 
-  const { image, bookName, author, category, review,tags,publisher,yearOfPublishing,rating,totalPages  } =
-    singleData || {};
-  
-    const handleSaveToRead = () => {
-        saveBooks(singleData, "Read");
-    };
+  const {
+    image,
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    publisher,
+    yearOfPublishing,
+    rating,
+    totalPages,
+  } = singleData || {};
 
-    const handleSaveToWishlist = () => {
-        saveBooks(singleData, "Wishlist");
-    };
-    return (
-         <Card className="w-full flex-col items-center md:flex-row my-[50px] gap-4 p-4 m-4 mt-[80px]">
+  const handleSaveToRead = () => {
+    saveBooks(singleData, "Read");
+  };
+
+  const handleSaveToWishlist = () => {
+    saveBooks(singleData, "Wishlist");
+  };
+  return (
+    <Card className="w-full flex-col items-center md:flex-row my-[50px] gap-4 p-4  mt-[80px]">
       <CardHeader
         shadow={false}
         floated={false}
@@ -45,48 +54,72 @@ const BookDetails = () => {
           className="h-full w-full object-cover bg-[#1313130D] p-12"
         />
       </CardHeader>
-      <CardBody className="space-y-2">
+      <CardBody className="space-y-2 p-4">
         <Typography variant="h4" color="blue-gray" className="mb-2">
-      {bookName}
-                </Typography>
-                
-        <Typography color="gray" className="font-normal border-b border-dashed p-2 w-full">
+          {bookName}
+        </Typography>
+
+        <Typography
+          color="gray"
+          className="font-normal border-b border-dashed p-2 w-full"
+        >
           By: {author}
         </Typography>
-        <Typography color="gray" className="font-normal border-b border-dashed p-2">
-        {category}
+        <Typography
+          color="gray"
+          className="font-normal border-b border-dashed p-2"
+        >
+          {category}
         </Typography>
-        
+
         <Typography color="gray" className="font-normal text-[#131313B2]">
-        <span className="text-[#131313] font-bold">Review: </span>    {review}
+          <span className="text-[#131313] font-bold">Review: </span> {review}
         </Typography>
-             <div className="font-normal border-b border-dashed p-2">
-  <div className="flex gap-3 items-center">
-    <span className="text-gray-800 font-bold">Tag: </span>
-    {tags && tags.map((tag, index) => ( 
-      <p className="text-green-500 bg-green-100 rounded-full p-2 px-6" key={index}>#{tag}</p>
-    ))}
-  </div>
-</div>
-                <Typography color="gray" className="font-normal text-[#131313]">
-  <span className="text-[#131313B2]">    Number of Pages: </span> {totalPages}
+        <div className="font-normal border-b border-dashed p-2">
+          <div className="flex gap-2 items-center">
+            <span className="text-gray-800 font-bold">Tag: </span>
+            {tags &&
+              tags.map((tag, index) => (
+                <p
+                  className="text-green-500 bg-green-100 rounded-full p-2 px-6"
+                  key={index}
+                >
+                  #{tag}
+                </p>
+              ))}
+          </div>
+        </div>
+        <Typography color="gray" className="font-normal text-[#131313]">
+          <span className="text-[#131313B2]"> Number of Pages: </span>{" "}
+          {totalPages}
         </Typography>
-                <Typography color="gray" className="font-normal text-[#131313]">
-  <span className="text-[#131313B2]">    Publisher: </span> {publisher}
+        <Typography color="gray" className="font-normal text-[#131313]">
+          <span className="text-[#131313B2]"> Publisher: </span> {publisher}
         </Typography>
-                <Typography color="gray" className="font-normal text-[#131313]">
-  <span className="text-[#131313B2]">    Year of Publishing: </span> {yearOfPublishing}
+        <Typography color="gray" className="font-normal text-[#131313]">
+          <span className="text-[#131313B2]"> Year of Publishing: </span>{" "}
+          {yearOfPublishing}
         </Typography>
-                <Typography color="gray" className="font-normal text-[#131313]">
-  <span className="text-[#131313B2]">    Rating: </span> {rating}
-                </Typography>
-              <div className="items-center flex-shrink flex gap-4">
-                    <button onClick={handleSaveToRead} className="self-center px-8 py-3 text-[#131313] rounded border border-solid border-[#1313134D]">Read</button>
-                    <button onClick={handleSaveToWishlist} className="self-center px-8 py-3 font-semibold rounded bg-[#59C6D2] text-white">Wishlist</button>
-                </div>
+        <Typography color="gray" className="font-normal text-[#131313]">
+          <span className="text-[#131313B2]"> Rating: </span> {rating}
+        </Typography>
+        <div className="items-center flex-shrink flex gap-4">
+          <button
+            onClick={handleSaveToRead}
+            className="self-center px-8 py-3 text-[#131313] rounded border border-solid border-[#1313134D]"
+          >
+            Read
+          </button>
+          <button
+            onClick={handleSaveToWishlist}
+            className="self-center px-8 py-3 font-semibold rounded bg-[#59C6D2] text-white"
+          >
+            Wishlist
+          </button>
+        </div>
       </CardBody>
     </Card>
-    );
+  );
 };
 
 export default BookDetails;
--- src/Pages/ExploreBooks.jsx
+++ src/Pages/ExploreBooks.jsx
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from "react";
 import useBooksData from "../Hooks/useBooksData";
 import ExploreBook from "../components/ExploreBook";
-import image from '../../src/assets/explorebook.jpg'
+import image from "../../src/assets/Book.jpg";
 const ExploreBooks = () => {
-    const [books, setBooks] = useState([]);
-    const { data } = useBooksData();
+  const [books, setBooks] = useState([]);
+  const { data } = useBooksData();
 
-    useEffect(() => {
-        setBooks(data);
-    }, [data]);
+  useEffect(() => {
+    setBooks(data);
+  }, [data]);
 
-    return (
-        <div className="p-4 mt-[80px] text-center m-4"> 
-            <h2 className="font-bold text-[40px]">Explore Books</h2>
-              <div className="hero bg-[#0d0d0d0d] mt-4 md:mt-[52px] md:py-[80px] rounded-[24px]">
-  <div className="hero-content flex-col lg:flex-row-reverse gap-4 lg:gap-[86px]">
-    <img src={image} className="max-w-[310px] mx-auto rounded-lg shadow-2xl p-4" />
-    <div className="max-w-[526px] mx-auto">
-      <h1 className="text-3xl pt-2 md:text-5xl font-bold pb-4">Hundreds of books picked for you</h1>
-                        <p className="text-lg md:text-xl pb-4">Explore a curated catalogue, completely free</p>
-     
-    </div>
-  </div>
-</div>
-
-            <div className="mt-6 md:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {books.map(book => <ExploreBook key={book.bookId} book={book}></ExploreBook>)}
-            </div>
+  return (
+    <div className="p-2 mt-[80px] text-center m-4">
+      <h2 className="font-bold text-[40px]">Explore Books</h2>
+      <div className="hero bg-[#0d0d0d0d] p-4 mt-6 md:mt-[52px] md:py-[80px] rounded-[24px]">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-4 lg:gap-[86px] pr-4">
+          <img src={image} className="max-w-[310px] rounded-lg shadow-2xl" />
+          <div className="max-w-[526px]">
+            <h1 className="text-5xl font-bold pb-10">
+              {" "}
+              Hundreds of books picked for you
+            </h1>
+            <p className="text-lg md:text-xl pb-4">
+              Explore a curated catalogue, completely free
+            </p>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="mt-6 md:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {books.map((book) => (
+          <ExploreBook key={book.bookId} book={book}></ExploreBook>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ExploreBooks;
